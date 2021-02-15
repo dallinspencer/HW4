d1a37e7fe4e48f5f7b8f662d37c719d59662a973
aslettz <zankaslett@gmail.com>
2021-02-15 14:52:53 -0700

    Working solution
    Made output of gmres more useful. Testing for the function is complete. Also created the popMatrices function for use with problem from part 1

--- gmres_runner.m
+++ gmres_runner.m
@@ -12,9 +12,11 @@ A=randi([10 100], 100, 100);
 n = 100;
 x0 = A(3,:)';
 M = eye(100);
+
 if det(A) ~=0
     b  = randi([10 100],100, 1);
     maxit = 100;
     tol=1e-1;
-    [xs,ys,~,~] = gmres_matlab(A,b,maxit,x0, M, n) ;
+    [sol,xs,ys,Vs,Hs] = gmres_matlab(A,b,maxit,x0, M, n);
 end
+diff = b - A*sol
