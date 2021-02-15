7ffa2898aa3a886501b07649a9e2d9c030b9b46d
Dallin <dallinspencer@gmail.com>
2021-02-15 11:43:52 -0700

    Stuff

--- HW4.py
+++ HW4.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy as sc
+from prob1 import popMatrices as pop
+import krypy as kry
 #Question 1
 
 
@@ -30,7 +32,9 @@ def mygmres(l,b,x0,n,M,A):
         #r0 = b - np.matmul(A,x0)
         e1 = np.zeros(l)
         e1[0] = 1
-        H,Q = arnoldi(A,l,b)
+        print(A)
+        print(np.shape(np.array([b,])))
+        H,Q = kry.utils.arnoldi(A,np.transpose(np.array([b,])))
         alpha = e1 * np.linalg.norm(r0)
         print("H = ", H)
         print("\nalpha = ", alpha, "\n")
@@ -48,3 +52,46 @@ print(x, 'Scipy rsolution')
 A = A.toarray()
 x = mygmres(23,b,x+1.5,3,[0],A)
 print(x)
+
+#Part 3
+n = 16
+M = np.identity(n)
+A,b = pop(n,1)
+x0 = np.array([1,1,1])
+test1 = np.array([mygmres(2,b,x0,n,M,A),mygmres(4,b,x0,n,M,A),mygmres(8,b,x0,n,M,A),mygmres(16,b,x0,n,M,A),mygmres(32,b,x0,n,M,A),mygmres(64,b,x0,n,M,A)])
+n = 32
+M = np.identity(n)
+A,b = pop(n,1)
+x0 = np.array([1,1,1])
+test2 = np.array([mygmres(2,b,x0,n,M,A),mygmres(4,b,x0,n,M,A),mygmres(8,b,x0,n,M,A),mygmres(16,b,x0,n,M,A),mygmres(32,b,x0,n,M,A),mygmres(64,b,x0,n,M,A)])
+n = 64
+M = np.identity(n)
+A,b = pop(n,1)
+x0 = np.array([1,1,1])
+test3 = np.array([mygmres(2,b,x0,n,M,A),mygmres(4,b,x0,n,M,A),mygmres(8,b,x0,n,M,A),mygmres(16,b,x0,n,M,A),mygmres(32,b,x0,n,M,A),mygmres(64,b,x0,n,M,A)])
+n = 128
+M = np.identity(n)
+A,b = pop(n,1)
+x0 = np.array([1,1,1])
+test4 = np.array([mygmres(2,b,x0,n,M,A),mygmres(4,b,x0,n,M,A),mygmres(8,b,x0,n,M,A),mygmres(16,b,x0,n,M,A),mygmres(32,b,x0,n,M,A),mygmres(64,b,x0,n,M,A)])
+
+n = 16
+M = np.identity(n)
+A,b = pop(n,n+1)
+x0 = np.array([1,1,1])
+test1 = np.array([mygmres(2,b,x0,n,M,A),mygmres(4,b,x0,n,M,A),mygmres(8,b,x0,n,M,A),mygmres(16,b,x0,n,M,A),mygmres(32,b,x0,n,M,A),mygmres(64,b,x0,n,M,A)])
+n = 32
+M = np.identity(n)
+A,b = pop(n,n+1)
+x0 = np.array([1,1,1])
+test2 = np.array([mygmres(2,b,x0,n,M,A),mygmres(4,b,x0,n,M,A),mygmres(8,b,x0,n,M,A),mygmres(16,b,x0,n,M,A),mygmres(32,b,x0,n,M,A),mygmres(64,b,x0,n,M,A)])
+n = 64
+M = np.identity(n)
+A,b = pop(n,n+1)
+x0 = np.array([1,1,1])
+test3 = np.array([mygmres(2,b,x0,n,M,A),mygmres(4,b,x0,n,M,A),mygmres(8,b,x0,n,M,A),mygmres(16,b,x0,n,M,A),mygmres(32,b,x0,n,M,A),mygmres(64,b,x0,n,M,A)])
+n = 128
+M = np.identity(n)
+A,b = pop(n,n+1)
+x0 = np.array([1,1,1])
+test4 = np.array([mygmres(2,b,x0,n,M,A),mygmres(4,b,x0,n,M,A),mygmres(8,b,x0,n,M,A),mygmres(16,b,x0,n,M,A),mygmres(32,b,x0,n,M,A),mygmres(64,b,x0,n,M,A)])
